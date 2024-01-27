aef82a4dcd6a7db86d4a67c3e19256d507c2da7d
vivekrai1999 <vivek.rai.2807@gmail.com>
2024-01-27 10:41:18 +0530

    Added Dynamic Search using OMDB

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const tempMovieData = [
   {
@@ -50,9 +50,16 @@ const tempWatchedData = [
 const average = (arr) =>
   arr.reduce((acc, cur, i, arr) => acc + cur / arr.length, 0);
 
+const KEY = '84552b0e'
+
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [movies, setMovies] = useState([]);
+  const [watched, setWatched] = useState([]);
+  useEffect(()=>{
+    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
+    .then(res => res.json())
+    .then(data => setMovies(data.Search))
+  },[])
   return (
     <>
       <NavBar>
@@ -195,7 +202,7 @@ function Logo(){
 
 function NumResults({movies}){
   return <p className="num-results">
-  Found <strong>{movies.length}</strong> results
+  Found <strong>{movies?.length}</strong> results
 </p>
 }
 
