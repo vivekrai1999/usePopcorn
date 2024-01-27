6113deec256c27ef95d933722e8d1472cb411531
vivekrai1999 <vivek.rai.2807@gmail.com>
2024-01-27 15:20:46 +0530

    Used Async/Await

--- src/App.jsx
+++ src/App.jsx
@@ -56,9 +56,12 @@ export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
   useEffect(()=>{
-    fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`)
-    .then(res => res.json())
-    .then(data => setMovies(data.Search))
+    async function fetchMovies(){
+      const res = await fetch(`http://www.omdbapi.com/?apikey=${KEY}&s=interstellar`);
+      const data = await res.json()
+      setMovies(data.Search)
+    }
+    fetchMovies();
   },[])
   return (
     <>
