9e270bc64b7169c354eb7a25e71b78fb23ae1254
vivekrai1999 <vivek.rai.2807@gmail.com>
2024-01-30 23:24:07 +0530

    Select search by default - useRef

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,17 @@
+import { useEffect, useRef } from "react"
+
 function Search({query, setQuery}){
+    const inputEl = useRef(null)
+    useEffect(()=>{
+      inputEl.current.focus()
+    },[])
     return <input
     className="search"
     type="text"
     placeholder="Search movies..."
     value={query}
     onChange={(e) => setQuery(e.target.value)}
+    ref={inputEl}
   />
   }
 
