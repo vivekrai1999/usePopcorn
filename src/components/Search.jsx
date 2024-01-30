332918b2532c3abf3935b3c72ba06dbf3de32947
vivekrai1999 <vivek.rai.2807@gmail.com>
2024-01-30 23:37:05 +0530

    Use enter to activate search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,7 +3,15 @@ import { useEffect, useRef } from "react"
 function Search({query, setQuery}){
     const inputEl = useRef(null)
     useEffect(()=>{
-      inputEl.current.focus()
+      function callback(e){
+        if(document.activeElement===inputEl.current) return;
+        if(e.code==='Enter'){
+          inputEl.current.focus()
+          setQuery('')
+        }
+      }
+      document.addEventListener('keydown',callback)
+      return document.addEventListener('keydown',callback)
     },[])
     return <input
     className="search"
